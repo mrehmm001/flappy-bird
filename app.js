5c2341eee1b2f464107b8730a375ee39a478cc52
mrehmm001 <muneeb73@outlook.com>
2021-02-01 20:20:40 +0000

    third commit

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded',()=>{
     let bird = document.querySelector('.bird');
+    let canvas=document.querySelector('.canvas');
     let birdBottom=500
 
     bird.style.bottom=birdBottom+'px'
@@ -11,13 +12,35 @@ document.addEventListener('DOMContentLoaded',()=>{
     }
     function control(e){
         if(e.keyCode===32){
-            console.log("test")
             jump(); 
         }
     }
     function jump(){
         birdBottom+=50; 
     }
+
+    function createObstacle(){
+        let obstacle=document.createElement('div');
+        obstacle.classList.add('obstacle');
+        canvas.appendChild(obstacle); 
+        let obstacleLeft=650;
+        obstacle.style.left=obstacleLeft+'px';
+        obstacle.style.top=65+'%';
+        function moveObstacle(){
+            obstacleLeft-=5;
+            obstacle.style.left=obstacleLeft+'px';
+            if(obstacleLeft==-10){
+                canvas.removeChild(obstacle);
+                clearInterval(timerId);
+            }
+
+        }
+        let timerId=setInterval(moveObstacle,20);  
+        setTimeout(createObstacle,2000); 
+    }
+    createObstacle();
+    
+
     document.addEventListener('keyup',control)  
     let timerID = setInterval(startGame,20);
 });
